4c284978084c08378dc27ce067a6e9f9bc921a72
Sinha-Ujjawal <https://github.com/Sinha-Ujjawal>
2022-05-27 20:56:29 +0530

    Init Repo

---
+++ README.md
@@ -0,0 +1,11 @@
+## TODO APP
+A simple TODO app in [Elm](https://elm-lang.org/)+[Elixir Phoenix Framework](https://phoenixframework.org/)
+
+## Getting Started
+1. Follow [Getting Started](./backend/README.md) of `backend`
+2. Follow [Getting Started](./frontend/README.md) of `frontend`
+
+## Copyrights & License
+@Copyright [Sinha-Ujjawal](https://github.com/Sinha-Ujjawal)
+
+Licensed under [MIT](./LICENSE)
---
+++ frontend/src/Counter.elm
@@ -0,0 +1,73 @@
+module Counter exposing (main)
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+
+-- Model
+
+
+type alias Model =
+    { counter : Int
+    }
+
+
+init : Model
+init =
+     { counter = 0 }
+
+
+
+-- Update
+
+
+type Msg
+    = CountUp
+    | CountDown
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        CountUp ->
+            { model | counter = model.counter + 1}
+
+        CountDown ->
+            { model | counter = model.counter - 1 }
+
+
+
+-- View
+
+
+showCount : Model -> Html Msg
+showCount model =
+    text (String.fromInt model.counter)
+
+
+countUpButton : Html Msg
+countUpButton =
+    button [onClick CountUp] [ text "+" ]
+
+
+countDownButton : Html Msg
+countDownButton =
+    button [onClick CountDown] [ text "-" ]
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ showCount model
+        , countUpButton
+        , countDownButton
+        ]
+
+main : Program () Model Msg
+main =
+    Browser.sandbox { init = init
+                    , update = update
+                    , view = view
+                    }
